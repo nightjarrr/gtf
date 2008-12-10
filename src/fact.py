2a89922bd3d77d1e049de425eba97fb8cf6e34fc
Alexey Nedilko <nightjarrr@users.noreply.github.com>
2008-12-10 17:51:39 +0200

    Added code files of Get The Facts project

---
+++ src/fact.py
@@ -0,0 +1,60 @@
+from taggable import Taggable
+import random
+
+
+class Fact(Taggable):
+    """
+        Defines the fact 'pattern' that can be applied to an actor.
+        The 'Fact' can be regarded as a logical predicate that operates on the set of Actors.
+        Predicate is a boolean function that is defined in an informal way,
+        e.g. as a human-readable sentence.
+        
+        The 'Fact' consists of two parts, that together form a predicate: a pattern and a set of logical rules.
+            *   The 'Pattern' is a human-readable representation of the 'Fact'. When a format string is substituted with the
+                name of a concrete actor, a meaningful sentence is formed.
+                
+            *   Rules define a formal logical function that determines the result of a predicate for a concrete actor.
+                Rules are defined in terms of actor's attributes, and impose restrictions on them.
+                
+                Example:
+                The rule {["fish", "bird"],!big} defines all actors, that are fish or bird, and are not big. 
+        
+        """
+    def __init__(self, format, tags = []):
+        Taggable.__init__(self, tags)
+        self.format = format
+
+    def isApplicableTo(self, actor):
+        return True
+
+    def getFactAbout(self, actor):
+        "Creates the concrete fact which is is built from the fact pattern and the specified actor"
+        return self.format % actor.name
+
+class FactChooser:
+    "Lets choose a random fact for an actor that matches actor's tags"
+
+    def __init__(self, facts):
+        self.facts = facts
+        random.seed();
+
+    def choose(self, actor):
+        applicableFacts = [fact for fact in self.facts if fact.isApplicableTo(actor)]
+        if len(applicableFacts) == 0:
+            return None
+        return random.choice(applicableFacts)
+
+class FactFormatter:
+    "Enables saving and loading Facts to string"
+
+    def read(self, factString):
+        if factString.find("|") == -1:
+            return Fact(factString.strip())
+        [format, tagList] = factString.split("|", 1)
+        tags = [tag.strip() for tag in tagList.split(",")]
+        return Fact(format, tags)
+
+    def write(self, fact):
+        if fact.hasTags():
+            return "%s| " % fact.format + ", ".join(fact.tags)
+        else: return fact.format
---
+++ src/main.py
@@ -0,0 +1,48 @@
+﻿import gettext
+from fact import *
+from actor import *
+import os
+
+#t = gettext.translation("getthefacts", "lang")
+_ = lambda msg: msg#t.gettext
+
+def readList(fileName, formatter):
+    list = []
+    try:
+        list = [formatter.read(line) for line in file(fileName).readlines() if not (line == "\n" or line.startswith("#"))]
+    finally:
+        return list
+
+if __name__ == "__main__":
+    # Not sure whether this should be permanent or eclipse-specific
+    os.chdir(os.path.dirname(__file__))
+    
+    print _("Welcome!")
+    print _("Loading...")
+    
+    actors = readList("../data/actors.txt", ActorFormatter())
+    facts = readList("../data/facts.txt", FactFormatter())
+    print _("Loaded %d facts and %d actors.") % (len(facts), len(actors))
+    print _("\nEnter the actor name:")
+    actorName = raw_input().strip()
+    foundActor = False
+    actor = None
+    for a in actors:
+        if a.name == actorName:
+            actor = a
+            foundActor = True
+            break
+    if not foundActor:
+        actor = Actor(actorName)
+
+    print actorName
+
+    chooser = FactChooser(facts)
+    chosenFact = chooser.choose(actor)
+    if chosenFact == None:
+        print _("Sorry, I don't know anything about %s.") % actorName
+    else:
+        print _("Here is something interesting:\n")
+        print chosenFact.getFactAbout(actor)
+    print _("\nPress 'Enter' to exit.")
+    raw_input()
