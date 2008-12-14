1ed2798e9714072c372584b5f5e171936ee982e6
Alexey Nedilko <nightjarrr@users.noreply.github.com>
2008-12-14 13:56:10 +0200

    Added the encoding mark (#coding=UTF-8) at the beginning of each source file.

---
+++ src/getthefacts.tests/ruleparser_tests.py
@@ -0,0 +1,37 @@
+# coding=UTF-8
+from getthefacts.actor import Actor
+from getthefacts.rules import RuleParser
+import unittest
+
+class RuleParserTests(unittest.TestCase):
+    def testParseNameRule(self):
+        name = "Winnie-The-Pooh"
+        p = RuleParser("@" + name)
+        rule = p.parse()
+        assert not rule.evaluate(Actor("Rabbit"))
+        assert rule.evaluate(Actor(name))
+
+    def testParseNameRuleRus(self):
+        name = "Винни-Пух"
+        p = RuleParser("@" + name)
+        rule = p.parse()
+        assert not rule.evaluate(Actor("Кролик"))
+        assert rule.evaluate(Actor(name))
+
+    def testParseNotRule(self):
+        name = "Winnie-The-Pooh"
+        p = RuleParser("!@" + name)
+        rule = p.parse()
+        assert rule.evaluate(Actor("Rabbit"))
+        assert rule.evaluate(Actor("Piglet"))
+        assert not rule.evaluate(Actor(name))
+
+    def testParseTagRule(self):
+        p = RuleParser("bear")
+        rule = p.parse()
+        assert not rule.evaluate(Actor("Piglet", ["small", "pig", "toy"]))
+        assert rule.evaluate(Actor("Winnie-The-Pooh", ["bear", "toy"]))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/getthefacts.tests/taggable_tests.py
+++ src/getthefacts.tests/taggable_tests.py
@@ -1,3 +1,4 @@
+# coding=UTF-8
 import unittest
 from getthefacts.taggable import Taggable
 
