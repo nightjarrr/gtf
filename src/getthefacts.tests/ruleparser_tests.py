c3927351a782b57f8d3028e2a470a67ec8a57e56
Alexey Nedilko <alexey_nedilko@lavabit.com>
2008-12-14 19:02:27 +0200

    Fixed two bugs in rules parser.  - Embedded And rules: ((tag2,tag3), tag1)  - Skipping whitespaces.

--- src/getthefacts.tests/ruleparser_tests.py
+++ src/getthefacts.tests/ruleparser_tests.py
@@ -1,5 +1,4 @@
 # coding=UTF-8
-from getthefacts.actor import Actor
 from getthefacts.rules import *
 import unittest
 
@@ -18,11 +17,48 @@ class NotRuleParserTests(unittest.TestCase):
         rule = p.parse()
         assert rule == NotRule(NameRule(name))
 
+    def testParseWhitespace(self):
+        name = "Winnie-The-Pooh"
+        p = RuleParser("!   @" + name)
+        rule = p.parse()
+        assert NotRule(NameRule(name)) == RuleParser("!   @" + name).parse()
+        assert NotRule(NameRule(name)) == RuleParser("!\t@" + name).parse()
+        assert NotRule(NameRule(name)) == RuleParser("! @" + name).parse()
+        assert NotRule(NameRule(name)) == RuleParser("  !   @" + name).parse()
+        assert NotRule(NameRule(name)) == RuleParser("!   @" + name + "    ").parse()
+        assert NotRule(NameRule(name)) == RuleParser("!   @" + name + "\t  ").parse()
+
 class TagRuleParserTests(unittest.TestCase):
     def testParse(self):
         p = RuleParser("bear")
         rule = p.parse()
         assert rule == TagRule("bear")
+
+class AndRuleParserTests(unittest.TestCase):
+    def testParse(self):
+        p = RuleParser("(bear, !@Winnie-The-Pooh, !toy)")
+        rule = p.parse()
+        assert rule == AndRule([TagRule("bear"),
+                                NotRule(NameRule("Winnie-The-Pooh")),
+                                NotRule(TagRule("toy"))])
+
+    def testParseEmbedded(self):
+        p = RuleParser("((@Baloo, !big), bear, !@Winnie-The-Pooh, !toy)")
+        rule = p.parse()
+        assert rule == AndRule([AndRule([NameRule("Baloo"),
+                                         NotRule(TagRule("big"))]),
+                                TagRule("bear"),
+                                NotRule(NameRule("Winnie-The-Pooh")),
+                                NotRule(TagRule("toy"))])
+
+    def testParseNegated(self):
+        p = RuleParser("!(@Baloo, !big)")
+        rule = p.parse()
+        assert rule == NotRule(AndRule([NameRule("Baloo"), NotRule(TagRule("big"))]))
+
+class OrRuleParserTests(unittest.TestCase):
+    pass
+    
 
 if __name__ == "__main__":
     unittest.main()
