d76f01d14e1079b8f4370d097e7e080b5bacaea8
Alexey Nedilko <nightjarrr@users.noreply.github.com>
2008-12-14 22:48:42 +0200

    fact.py: Updated the module docstring.

--- src/getthefacts/fact.py
+++ src/getthefacts/fact.py
@@ -4,23 +4,24 @@ import random
 
 
 class Fact(Taggable):
+
     """
-        Defines the fact 'pattern' that can be applied to an actor.
-        The 'Fact' can be regarded as a logical predicate that operates on the set of Actors.
-        Predicate is a boolean function that is defined in an informal way,
-        e.g. as a human-readable sentence.
-        
-        The 'Fact' consists of two parts, that together form a predicate: a pattern and a set of logical rules.
-            *   The 'Pattern' is a human-readable representation of the 'Fact'. When a format string is substituted with the
-                name of a concrete actor, a meaningful sentence is formed.
-                
-            *   Rules define a formal logical function that determines the result of a predicate for a concrete actor.
-                Rules are defined in terms of actor's attributes, and impose restrictions on them.
-                
-                Example:
-                The rule (["fish", "bird"],!big) defines all actors, that are fish or bird, and are not big.
-        
-        """
+    Defines the fact 'pattern' that can be applied to an actor.
+    The 'Fact' can be regarded as a logical predicate that operates on the set of Actors.
+    Predicate is a boolean function that is defined in an informal way,
+    e.g. as a human-readable sentence.
+
+    The 'Fact' consists of two parts, that together form a predicate: a pattern and a set of logical rules.
+        *   The 'Pattern' is a human-readable representation of the 'Fact'. When a format string is substituted with the
+            name of a concrete actor, a meaningful sentence is formed.
+
+        *   Rules define a formal logical function that determines the result of a predicate for a concrete actor.
+            Rules are defined in terms of actor's attributes, and impose restrictions on them.
+
+            Example:
+            The rule (["fish", "bird"],!big) defines all actors, that are fish or bird, and are not big.
+    """
+
     def __init__(self, format, tags = []):
         Taggable.__init__(self, tags)
         self.format = format
