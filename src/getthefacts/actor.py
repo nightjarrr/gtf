f17e68661d68e75553fb58166b79fac4404f1b82
Alexey Nedilko <alexey_nedilko@lavabit.com>
2008-12-15 06:20:57 +0200

    actor.py: Updated docstring documentation.

--- src/getthefacts/actor.py
+++ src/getthefacts/actor.py
@@ -2,15 +2,21 @@
 from taggable import Taggable
 
 class Actor(Taggable):
-    ""
+
+    """Object that can be a character of fact."""
+
     def __init__(self, name, tags = []):
+        """Initialize new instance of Actor."""
         Taggable.__init__(self, tags)
         self.name = name
+
         
 class ActorFormatter:
-    "Enables saving and loading the Actor to string"
+
+    "Read or write Actor to string"
     
     def read(self, actorString):
+        """Read Actor from string representation."""
         if actorString.find("|") == -1:
             return Actor(actorString.strip())
         [actorName, tagList] = actorString.split("|", 1)
@@ -18,6 +24,7 @@ class ActorFormatter:
         return Actor(actorName.strip(), tags)
     
     def write(self, actor):
+        """Write Actor to string representation."""
         if actor.hasTags():
             return "%s| " % actor.name + ", ".join(actor.tags)
         else: return actor.name
