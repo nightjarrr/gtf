cca7fd5696c80ebcca0213f8a05bd56c8b510c80
Alexey Nedilko <alexey_nedilko@lavabit.com>
2008-12-15 06:16:15 +0200

    taggable.py: Updated docstring documentation.

--- src/getthefacts/taggable.py
+++ src/getthefacts/taggable.py
@@ -1,17 +1,23 @@
 # coding=UTF-8
 
 class Taggable:
-    "Defines an object that can be assigned with several descriptive tags"
+
+    """Object that can be labelled with tags."""
+    
     def __init__(self, tags):
+        """Initialize new instance of Taggable."""
         self.tags = tags
     
     def hasTags(self):
+        """Return True if any tags are assigned to this instance."""
         return len(self.tags) > 0
     
     def isTaggedWith(self, tag):
+        """Return True if this instance is tagged with specified tag."""
         return tag in self.tags
         
     def isTaggedWithAny(self, tags):
+        """Return True if this instance is tagged with any of specified tags."""
         for tag in tags:
             if self.isTaggedWith(tag): return True
         return False
