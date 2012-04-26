16ec41ed67db12dd2303361781b4e70617827ebf
Alexey Nedilko <nightjarrr@users.noreply.github.com>
2012-04-26 22:05:26 +0300

    Added some details about Jinja extension Actor.

--- src/getthefacts/fact/jinja.py
+++ src/getthefacts/fact/jinja.py
@@ -6,4 +6,25 @@ from .. import rules
 from jinja2.ext import Extension
 
 class ActorExtension(Extension):
-	pass
+    """
+    The Jinja2 extension that allows to define actor rules in Jinja template
+    Sample usage is as follows:
+
+        {% actor 'author', '(bear, big, !toy, !@Baloo)' %}
+
+    Here 'author' is the actor name that can be used further throughout the template,
+    and '(bear, big, !toy, !@Baloo)' is a rule definition for the actor.
+    Rule sting is optional, and in case it is missing, any actor is deemed suitable
+
+        {% actor 'author' %}
+
+    """
+
+    tags = set(["actor"])
+
+	def __init__(self, environment):
+	    super(ActorExtension, self).__init__(environment)
+
+    def parse(self, parser):
+        pass
+
