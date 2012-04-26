a6133628cd40665c43c09f69730f50e1a61d7326
Alexey_Nedilko <nightjarrr@users.noreply.github.com>
2012-04-26 18:25:10 +0300

    Added module for Jinja-based fact templates and unit tests for it.

---
+++ src/getthefacts/fact/jinja.py
@@ -0,0 +1,9 @@
+# coding=UTF-8
+
+from . import *
+from .. import rules
+
+from jinja2.ext import Extension
+
+class ActorExtension(Extension):
+	pass
---
+++ src/getthefacts/tests/jinja_facts_tests.py
@@ -0,0 +1,9 @@
+# coding=UTF-8
+import unittest
+from getthefacts.fact.jinja import ActorExtension
+from getthefacts.actor import Actor
+
+class JinjaFactTemplate(unittest.TestCase):
+	pass
+
+
