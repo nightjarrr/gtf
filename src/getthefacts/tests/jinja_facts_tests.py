95bada238db51c93703d534874cc238d3b4e10ff
Alexey Nedilko <nightjarrr@users.noreply.github.com>
2012-04-26 23:31:06 +0300

    Implemented ActorExtension.

--- src/getthefacts/tests/jinja_facts_tests.py
+++ src/getthefacts/tests/jinja_facts_tests.py
@@ -2,8 +2,24 @@
 import unittest
 from getthefacts.fact.jinja import ActorExtension
 from getthefacts.actor import Actor
+from getthefacts.rules import *
+from jinja2 import Environment
 
-class JinjaFactTemplate(unittest.TestCase):
-	pass
+class JinjaFactTemplateTests(unittest.TestCase):
+    def testActorSyntax(self):
+        e = Environment(extensions = [ActorExtension])
+        template = e.from_string(' {% actor "author", "(old, beard)" %} ')
+        assert e.actorPlaceholders is not None
+        assert len(e.actorPlaceholders) == 1
+        assert e.actorPlaceholders[0].index == 'author'
+        assert e.actorPlaceholders[0].rule == AndRule([TagRule('old'), TagRule('beard')])
+
+    def testActorWithoutRulesSyntax(self):
+        e = Environment(extensions = [ActorExtension])
+        template = e.from_string(' {% actor "author" %}')
+        assert e.actorPlaceholders is not None
+        assert len(e.actorPlaceholders) == 1
+        assert e.actorPlaceholders[0].index == 'author'
+        assert e.actorPlaceholders[0].rule == TrueRule()
 
 
