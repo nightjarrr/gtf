addd9e7c1ec6d5850f8a4955be8c4c50adfc2aac
Alexey_Nedilko <Alexey_Nedilko@localhost>
2012-04-27 14:57:59 +0300

    Added a test for multiple actor definitions in a Jinja template.

--- src/getthefacts/tests/jinja_facts_tests.py
+++ src/getthefacts/tests/jinja_facts_tests.py
@@ -22,4 +22,20 @@ class JinjaFactTemplateTests(unittest.TestCase):
         assert e.actorPlaceholders[0].index == 'author'
         assert e.actorPlaceholders[0].rule == TrueRule()
 
+    def testMultipleActorSyntax(self):
+        e = Environment(extensions = [ActorExtension])
+        template = e.from_string("""
+                {% actor "author1", "(old, beard)" %}
+                {% actor "author2", "(!old, beard)" %}
+                {% actor "author3", "(old, !beard)" %}
+                """)
+        assert e.actorPlaceholders is not None
+        assert len(e.actorPlaceholders) == 3
+        assert e.actorPlaceholders[0].index == 'author1'
+        assert e.actorPlaceholders[0].rule == AndRule([TagRule('old'), TagRule('beard')])
 
+        assert e.actorPlaceholders[1].index == 'author2'
+        assert e.actorPlaceholders[1].rule == AndRule([NotRule(TagRule('old')), TagRule('beard')])
+
+        assert e.actorPlaceholders[2].index == 'author3'
+        assert e.actorPlaceholders[2].rule == AndRule([TagRule('old'), NotRule(TagRule('beard'))])
